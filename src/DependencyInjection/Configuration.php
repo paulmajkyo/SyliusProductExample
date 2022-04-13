9da5302cf05120f247af56747fdc6de501f848eb
Paweł Maj <pawel.maj@gamfi.pl>
2022-04-13 20:40:20 +0200

    show text after details

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -14,7 +14,7 @@ final class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder('acme_sylius_example_plugin');
+        $treeBuilder = new TreeBuilder('wiseb2b_sylius_product_plugin');
         $rootNode = $treeBuilder->getRootNode();
 
         return $treeBuilder;
