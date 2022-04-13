d3390a1de974ff397dbfff174c2d39ca9df2847b
Paweł Maj <pawel.maj@gamfi.pl>
2022-04-13 20:41:34 +0200

    show text after details

--- src/DependencyInjection/Wiseb2bSyliusProductExtension.php
+++ src/DependencyInjection/Wiseb2bSyliusProductExtension.php
@@ -5,10 +5,8 @@ declare(strict_types=1);
 namespace Wiseb2b\SyliusProductPlugin\DependencyInjection;
 
 use Symfony\Component\Config\Definition\ConfigurationInterface;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 final class Wiseb2bSyliusProductExtension extends Extension
 {
@@ -17,10 +15,7 @@ final class Wiseb2bSyliusProductExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $config = $this->processConfiguration($this->getConfiguration([], $container), $configs);
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
 
-        $loader->load('services.xml');
     }
 
     public function getConfiguration(array $config, ContainerBuilder $container): ConfigurationInterface
